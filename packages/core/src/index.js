41110ba342f9992cfbb32e20e77958fa7f57cac2
Denis Grenader <denisg@gmail.com>
2020-05-16 16:15:41 -0700

    Publish ES5 code to NPM

---
+++ packages/core/src/index.js
@@ -0,0 +1,11 @@
+/* @flow strict */
+import { Optional, Some, None, Match } from './optional.js';
+import { ImmutableClass } from './immutableClass.js';
+
+export {
+  Optional,
+  Some,
+  None,
+  Match,
+  ImmutableClass
+};
